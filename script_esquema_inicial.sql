bf759d20728007cc2d92a0b87a34d68226bb71ea
DESKTOP-L3ELK29\gabri <groldan@est.frba.utn.edu.ar>
2017-06-12 04:17:58 -0300

    Facturas, rendiciones, viajes y puto el que lee

--- script_esquema_inicial.sql
+++ script_esquema_inicial.sql
@@ -1,4 +1,4 @@
---###########################################################################
+--#####--###########################################################################
 --###########################################################################
 -- SCRIPT DE MIGRACION Y CREACION DE OBJETOS NECESARIOS
 -- GRUPO: DAVID_Y_LOS_COCODRILOS
@@ -72,6 +72,10 @@ DROP TABLE DAVID_Y_LOS_COCODRILOS.MODELO
 
 IF OBJECT_ID('DAVID_Y_LOS_COCODRILOS.MARCA') IS NOT NULL
 DROP TABLE DAVID_Y_LOS_COCODRILOS.MARCA
+
+IF OBJECT_ID('DAVID_Y_LOS_COCODRILOS.PERIODO') IS NOT NULL
+DROP TABLE DAVID_Y_LOS_COCODRILOS.PERIODO
+
 
 
 --PROCEDURES
@@ -182,6 +186,16 @@ GO
 --###########################################################################
 --###########################################################################
 
+--------------------------------------
+-------------TABLA PERIODO------------
+--------------------------------------
+CREATE TABLE DAVID_Y_LOS_COCODRILOS.PERIODO (
+	PERIODO_ANIO  	int,
+	PERIODO_MES 	int,
+	PERIODO_INICIO  datetime,
+	PERIODO_FIN datetime,
+	primary key (PERIODO_ANIO, PERIODO_MES)
+);
 
 --------------------------------------
 -------------TABLA ROL----------------
@@ -371,10 +385,12 @@ CREATE TABLE DAVID_Y_LOS_COCODRILOS.RENDICION (
 ----------TABLA ITEM RENDICION----------
 ----------------------------------------
 CREATE TABLE DAVID_Y_LOS_COCODRILOS.ITEM_RENDICION (
-	ITEM_ID numeric(18,0),
-	RENDICION_ID numeric(18,0),
-	RENDICION_VIAJE_ID char(8),
-	primary key(RENDICION_ID, RENDICION_VIAJE_ID)
+	ITEMR_IMPORTE numeric(18,2),
+	ITEMR_RENDICION numeric(18,0),
+	ITEMR_VIAJE char(8),
+	primary key(ITEMR_RENDICION, ITEMR_VIAJE),
+	foreign key (ITEMR_RENDICION) references DAVID_Y_LOS_COCODRILOS.RENDICION,
+	foreign key (ITEMR_VIAJE) references DAVID_Y_LOS_COCODRILOS.VIAJE,
 );
 
 
@@ -383,13 +399,12 @@ CREATE TABLE DAVID_Y_LOS_COCODRILOS.ITEM_RENDICION (
 ---------------------------------------
 CREATE TABLE DAVID_Y_LOS_COCODRILOS.FACTURA (
 	FACTURA_CLIENTE_ID char(8),
-	FACTURA_ID numeric(18,0),
-	FACTURA_TIPO char(8), --no se que es
-	FACTURA_FECHA_INICIO datetime,
-	FACTURA_FECHA_FIN datetime,
+	FACTURA_ID numeric(18) IDENTITY(1,1),
+	FACTURA_ANIO int,
+	FACTURA_PERIODO int,
 	FACTURA_IMPORTE_TOTAL numeric(18,2)
-	primary key (FACTURA_ID, FACTURA_CLIENTE_ID)
---	foreign key (FACTURA_CLIENTE_ID) references DAVID_Y_LOS_COCODRILOS.CLIENTE  todavia no esta la tabla CLIENTE
+	primary key (FACTURA_ID)
+	foreign key (FACTURA_CLIENTE_ID) references DAVID_Y_LOS_COCODRILOS.CLIENTE
 );
 
 
@@ -397,11 +412,10 @@ CREATE TABLE DAVID_Y_LOS_COCODRILOS.FACTURA (
 ----------------TABLA ITEM FACTURA-------------------
 -----------------------------------------------------
 CREATE TABLE DAVID_Y_LOS_COCODRILOS.ITEM_FACTURA (
-	VIAJE_ID char(8),
-	CLIENTE_ID char(8),
-	FACTURA_ID numeric(18,0),
-	FACTURA_TIPO char(8),
-	primary key (FACTURA_ID, VIAJE_ID)
+	ITEMF_VIAJE numeric(18,0),
+	ITEMF_FACTURA numeric(18,0),
+	ITEMF_IMPORTE numeric(18,2)
+	primary key (ITEMF_VIAJE, ITEMF_FACTURA)
 );
 
 GO
@@ -625,11 +639,29 @@ GO
 EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_TURNO')
 GO
 
+-----------------------------------------------------
+----------------MIGRACION PERIODO---------------------
+-----------------------------------------------------
+CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_PERIODO
+AS 
+BEGIN
+
+	INSERT INTO DAVID_Y_LOS_COCODRILOS.PERIODO (
+		PERIODO_ANIO,
+		PERIODO_MES,
+		PERIODO_INICIO,
+		PERIODO_FIN
+	)	(SELECT YEAR(Factura_Fecha_Inicio), MONTH(Factura_Fecha_Inicio), Factura_Fecha_Inicio, Factura_Fecha_Fin
+		from gd_esquema.Maestra 
+		group by Factura_Fecha_Inicio, Factura_Fecha_Fin)
+END
+GO
+
 
 -----------------------------------------------------
 ------------------MIGRACION VIAJE--------------------
 -----------------------------------------------------
-CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_VIAJE 
+CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_VIAJE
 AS
 BEGIN
 
@@ -642,29 +674,24 @@ BEGIN
 		VIAJE_FECHA_FIN,
 		VIAJE_CLIENTE_ID
 	)	SELECT	m.Auto_Patente,
-				(	SELECT c.USUARIO_ID
-					FROM DAVID_Y_LOS_COCODRILOS.CHOFER c join DAVID_Y_LOS_COCODRILOS.AUTOMOVIL a
-						 on c.USUARIO_ID = a.AUTOMOVIL_CHOFER
-					WHERE m.Auto_Patente = a.AUTOMOVIL_PATENTE
+				(	SELECT USUARIO_ID
+					FROM DAVID_Y_LOS_COCODRILOS.USUARIO
+					WHERE m.Chofer_Dni = USUARIO_DNI
 				),
 				(	SELECT t.TURNO_ID
 					FROM DAVID_Y_LOS_COCODRILOS.TURNO t
 					WHERE t.TURNO_HORA_INICIO = m.Turno_Hora_Inicio 
 							and t.TURNO_HORA_FIN = m.Turno_Hora_Fin
 				),
-				m.Viaje_Cant_Kilometros,
-				'1998-01-02 00:00:00.000',  --como sacar fecha inicio viaje ?
-				'1998-01-02 00:00:00.000',  --como sacar fecha fin viaje? 
+				MAX(m.Viaje_Cant_Kilometros),
+				m.Viaje_Fecha,  
+				m.Viaje_Fecha,  
 				(	SELECT c.CLIENTE_USUARIO 
 					FROM DAVID_Y_LOS_COCODRILOS.CLIENTE c join DAVID_Y_LOS_COCODRILOS.USUARIO u on c.CLIENTE_USUARIO = u.USUARIO_ID
-					WHERE m.Cliente_Telefono = u.USUARIO_TEL   --falta definiar la tabla Usuario por eso rompe
+					WHERE m.Cliente_Telefono = u.USUARIO_TEL   
 				)
 		FROM gd_esquema.Maestra m
-
-		--Si puedo sacar el userID desde la tabla usuario, para que me sirve la tabla admin y tabla cliente?
-		--no hacen absolutamente nada.
-		--El rol no podria estar en la tabla usuario? Chofer si tiene sentido porque tiene asignado un vehiculo
-
+		group by m.Chofer_Dni, m.Cliente_Dni, m.Auto_Patente, m.Turno_Hora_Inicio, m.Turno_Hora_Fin, m.Viaje_Fecha
 END
 GO
 
@@ -672,14 +699,13 @@ EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_VIAJE')
 GO
 
 
-/*  LO QUE SIGUE ESTA TODO EN NULL, QUE SE HACE CON ESTO? SE GENERAN DATOS O SE CARGAN CON ISNULL Y UN VALOR DE REEMPLAZO?
+
 -----------------------------------------------------
 ---------------------RENDICION-----------------------
 -----------------------------------------------------
 CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_RENDICION 
 AS
 BEGIN
-	
 	INSERT INTO DAVID_Y_LOS_COCODRILOS.RENDICION (
 		RENDICION_ID,
 		RENDICION_FECHA,
@@ -688,15 +714,14 @@ BEGIN
 		RENDICION_IMPORTE
 	)	SELECT	m.Rendicion_Nro,
 				m.Rendicion_Fecha,
-				'choferID',   --reemplazar por el select de abajo cuando este la tabla USUARIO
---				(	SELECT c.USUARIO_ID
---					FROM DAVID_Y_LOS_COCODRILOS.CHOFER c join DAVID_Y_LOS_COCODRILOS.USUARIO u on c.USUARIO_ID = u.USUARIO_ID
---					WHERE u.USUARIO_TELEFONO = m.Chofer_Telefono
---				),
+				(SELECT u.USUARIO_ID
+					FROM DAVID_Y_LOS_COCODRILOS.USUARIO u
+					WHERE u.USUARIO_DNI = m.Chofer_DNI),
 				DAVID_Y_LOS_COCODRILOS.fGetTurno(m.Turno_Hora_Inicio, m.Turno_Hora_Fin),
-				m.Rendicion_Importe
+				MAX(m.Rendicion_Importe)
 		FROM gd_esquema.Maestra m
-
+		where Rendicion_Nro > 0
+		group by m.Rendicion_Nro, m.Rendicion_Fecha, m.Chofer_DNI, m.Turno_Hora_Inicio, m.Turno_Hora_Fin;
 END
 GO
 
@@ -704,59 +729,100 @@ EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_RENDICION')
 GO
 
 
------------------------------------------------------
------------------ITEM RENDICION----------------------
------------------------------------------------------
---ESTO SI NO ME EQUIVOCO ES UN TRIGGER CUANDO SE CARGA UN VIAJE 
---PORQUE ES UNA TABLA APARTE Y NO ES LA TABLA VIAJE EL ITEM DE LA RENDICION DEL CHOFER?
-
 
 -----------------------------------------------------
 ----------------------FACTURA------------------------
 -----------------------------------------------------
-CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIRGACION_FACTURA 
+CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_FACTURA 
 AS
 BEGIN
 
 	INSERT INTO DAVID_Y_LOS_COCODRILOS.FACTURA (
-		FACTURA_ID,
 		FACTURA_CLIENTE_ID,
-		FACTURA_TIPO,
-		FACTURA_FECHA_INICIO,
-		FACTURA_FECHA_FIN,
+		FACTURA_ID,
+		FACTURA_ANIO,
+		FACTURA_PERIODO,
 		FACTURA_IMPORTE_TOTAL
-	)	SELECT	m.Factura_Nro,
-				'clientID' --sacar cuando este la tabla CLIENTE y USUARIO por el select de abajo
---				(	SELECT c.USUARIO_ID
---					FROM DAVID_Y_LOS_COCODRILOS.CLIENTE c join DAVID_Y_LOS_COCODRILOS.USUARIO on c.USUARIO_ID = u.USUARIO_ID
---					WHERE m.Cliente_Telefono = u.USUARIO_TELEFONO
---				),
-				'tipo', --nose si este campo hay que sacarlo, no hay forma de calcularlo
-				m.Factura_Fecha_Inicio,
-				m.Factura_Fecha_Fin,
-				(	SELECT sum(v.VIAJE_CANTIDAD_KM * t.TURNO_VALOR_KILOMETRO)
-					FROM DAVID_Y_LOS_COCODRILOS.ITEM_FACTURA i 
-						 join DAVID_Y_LOS_COCODRILOS.VIAJE v on i.VIAJE_ID = v.VIAJE_ID
-						 join DAVID_Y_LOS_COCODRILOS.TURNO t on v.VIAJE_TURNO_ID = t.TURNO_ID
-					GROUP BY i.FACTURA_ID
-				)
+	)	(SELECT
+				(SELECT c.USUARIO_ID FROM DAVID_Y_LOS_COCODRILOS.CLIENTE c join DAVID_Y_LOS_COCODRILOS.USUARIO on c.USUARIO_ID = u.USUARIO_ID WHERE m.Cliente_Telefono = u.USUARIO_TELEFONO),
+				m.Factura_Nro,
+				'B',
+				YEAR(m.Factura_Fecha_Inicio),
+				MONTH(m.Factura_Fecha_Inicio),
+				0
 		FROM gd_esquema.Maestra m
+		group by m.Cliente_Telefono, m.Factura_Nro, m.Factura_Fecha_Inicio
+		)
 
 END
 GO
 
 EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_FACTURA')
+GO
+
+
+
+-----------------------------------------------------
+-----------------ITEM RENDICION----------------------
+-----------------------------------------------------
+CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_ITEM_RENDICION 
+AS
+BEGIN
+	INSERT INTO DAVID_Y_LOS_COCODRILOS.ITEM_RENDICION (ITEMR_IMPORTE, ITEMR_RENDICION, ITEMR_VIAJE)
+		(SELECT	Rendicion_Importe, RENDICION_ID, VIAJE_ID
+		FROM (gd_esquema.Maestra JOIN DAVID_Y_LOS_COCODRILOS.RENDICION ON Rendicion_Nro = RENDICION_ID) JOIN DAVID_Y_LOS_COCODRILOS.VIAJE ON VIAJE_CHOFER_ID = RENDICION_CHOFER_ID AND VIAJE_CLIENTE_ID = RENDICION_CLIENTE_ID AND VIAJE_TURNO_ID = RENDICION_TURNO_ID AND VIAJE_FECHA_INICIO = Viaje_Fecha
+		where Rendicion_Nro > 0)
+END
+GO
+
+EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_ITEM_RENDICION')
+GO
+
 
 
 -----------------------------------------------------
 -----------------ITEM FACTURA------------------------
 -----------------------------------------------------
---TRIGGER QUE ACTUALIZA CUANDO SE CARGA UN VIAJE IGUAL QUE ITEM RENDICION
+CREATE PROCEDURE DAVID_Y_LOS_COCODRILOS.MIGRACION_ITEM_FACTURA 
+AS
+BEGIN
 
+	DECLARE @importeTotal numeric(18,2)
+	DECLARE @importe numeric(18,2)
+	DECLARE @factura numeric(18)
+	DECLARE @viaje numeric(18)
+	DECLARE @facturaAnterior numeric(18)
 
-*/
+	DECLARE clineas CURSOR FOR 
+	SELECT	((MAX(Viaje_Cant_Kilometros)* Turno_Valor_Kilometro)+ Turno_Precio_Base), FACTURA_ID, VIAJE_ID
+		FROM (gd_esquema.Maestra JOIN DAVID_Y_LOS_COCODRILOS.FACTURA ON Factura_Nro = FACTURA_ID) JOIN DAVID_Y_LOS_COCODRILOS.VIAJE ON VIAJE_CLIENTE_ID = FACTURA_CLIENTE_ID AND VIAJE_FECHA_INICIO = Viaje_Fecha
+		WHERE Factura_Nro > 0
+		GROUP BY Chofer_Dni, Cliente_Dni, Auto_Patente, Turno_Hora_Inicio, Turno_Precio_Base, Turno_Valor_Kilometro, Viaje_Fecha
 
+	OPEN clineas
+	FETCH NEXT clineas INTO @importe, @factura, @viajes
+	set @facturaAnterior = @factura
 
+	WHILE @@FETCH_STATUS = 0  
+	BEGIN  
+		IF @factura != @facturaAnterior
+		BEGIN
+			UPDATE DAVID_Y_LOS_COCODRILOS.ITEM_FACTURA SET FACTURA_IMPORTE = @importeTotal WHERE FACTURA_ID = @facturaAnterior
+			set @facturaAnterior = @factura
+			set @importeTotal = 0
+		END
+
+		set @importeTotal = @importeTotal + @importe
+		FETCH NEXT clineas INTO @importe, @factura, @viajes
+		INSERT INTO DAVID_Y_LOS_COCODRILOS.ITEM_FACTURA values (@viaje, @factura, @importe)
+
+	END
+
+END
+GO
+
+EXEC('DAVID_Y_LOS_COCODRILOS.MIGRACION_ITEM_FACTURA ')
+GO
 
 
 
