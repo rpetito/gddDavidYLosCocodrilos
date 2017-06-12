67bd42295fb4e5d51da2c8fa700f2e854304ffc6
nicogaray <nicolas.jm.garay@gmail.com>
2017-06-11 22:14:34 -0300

    conexion a la DB

--- UberFrba/Logeo.Designer.cs
+++ UberFrba/Logeo.Designer.cs
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // acceptButton
             // 
-            this.button1.Location = new System.Drawing.Point(61, 126);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ingresar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.acceptButton.Location = new System.Drawing.Point(61, 126);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 0;
+            this.acceptButton.Text = "Ingresar";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // userTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.userTextBox.Location = new System.Drawing.Point(88, 15);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userTextBox.TabIndex = 1;
             // 
-            // textBox2
+            // passwordTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 42);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.passwordTextBox.Location = new System.Drawing.Point(88, 42);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 2;
             // 
             // comboBox1
             // 
@@ -105,9 +105,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.acceptButton);
             this.Name = "Logeo";
             this.Text = "Inicio de sesión";
             this.ResumeLayout(false);
@@ -117,9 +117,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
--- UberFrba/Logeo.cs
+++ UberFrba/Logeo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,10 +18,46 @@ namespace UberFrba
             InitializeComponent();
         }
 
+        public string Password { get; set; }
+
+        public Boolean isValidPassword(string password)
+        {
+            string hashedPassword = hashPassword(password);
+            return this.Password.Equals(hashedPassword);
+        }
+
+        private string hashPassword(string password)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] hash = System.Security.Cryptography.SHA256.Create().ComputeHash(data);
+
+            return BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
+            String pUsername = userTextBox.Text;
+            String pContrasenia = passwordTextBox.Text;
+            Int32 id = 0;
+            Int32 intentos = 0;
+
+
+            try
+            {
+                SqlConnection Conexion = BaseDeDatos.ObternerConexion();
+                MessageBox.Show("estamos conectados");
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "there was an issue!");
+
+            }
             Menu menu = new Menu();
             menu.ShowDialog();
+
         }
     }
 }
